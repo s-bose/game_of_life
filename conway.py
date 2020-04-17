35f33efbdeadace88cba2cd3e6c4571eb9c180ed
Shiladitya Basu <shiladitya.31.z@gmail.com>
2020-04-18 01:05:50 +0530

    rulesets properly implemented

--- conway.py
+++ conway.py
@@ -49,43 +49,59 @@ class button:
 
         
 
-grid = []
-for row in range(rows):
-    grid.append([])
-    for column in range(rows):
-        grid[row].append(0)  
+def create_grid(size):
+    grid = []
+    for i in range(size):
+        grid.append([])
+        for j in range(size):
+            grid[i].append(0)  
+    return grid
 
-def generate(grid):
+
+
+main_grid = create_grid(rows)    # our main grid
+
+
+def generate(grid):     # generation of next iteration grid
     row = len(grid)
-    for i in range(1, row-1):
-        for j in range(1, row-1):
-            alive_neighbours = []
+
+
+    aux = create_grid(row)     # create a separate grid
+
+    
+    # do not update the current main grid until all the cells are updated
+
+    for i in range(row):    # include boundaries, we use modulus arithmetic
+        for j in range(row):
+            alive_neighbours = 0
             for x in range(-1, 2):
                 for y in range(-1, 2):
                     if not (x == 0 and y == 0): #exclude same cell.
-                        if (grid[i+x][j+y] == 1): #only include alive neighbours
-                            alive_neighbours.append(grid[i+x][j+y])
+                        r = (i + x + row) % row
+                        c = (j + y + row) % row
+                        if (grid[r][c] == 1): #only include alive neighbours
+                            alive_neighbours += 1
 
             #count neighbours
             if (grid[i][j] == 1):
 
                 #first case: Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-                if (len(alive_neighbours) < 2):
-                    grid[i][j] = 0
+                if (alive_neighbours < 2):
+                    aux[i][j] = 0
                 
                 #second case: Any live cell with two or three live neighbours lives on to the next generation.
-                elif (len(alive_neighbours) <= 3):
-                    grid[i][j] = 1
+                elif (alive_neighbours <= 3):
+                    aux[i][j] = 1
                 
                 #third case: Any live cell with more than three live neighbours dies, as if by overpopulation.
-                elif (len(alive_neighbours) > 3):
-                    grid[i][j] = 0
+                elif (alive_neighbours > 3):
+                    aux[i][j] = 0
             
             #fourth case: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-            elif (grid[i][j] == 0 and len(alive_neighbours) == 3):
-                grid[i][j] = 1
+            elif (grid[i][j] == 0 and alive_neighbours == 3):
+                aux[i][j] = 1
 
-    return grid
+    return aux
 
  
 
@@ -96,37 +112,35 @@ upload_button = button((btn_width + margin) * 2, screen_width, btn_width, btn_he
 
 def update_grid(grid, start_build):
 
-        if start_build  == True:
-            grid = generate(grid)
-        # Draw the grid
-        for row in range(rows):
-                for column in range(rows):
-                    color = WHITE
-                    if (grid[row][column] == 1):
-                        color = BLACK
-                    pygame.draw.rect(
-                        screen,
-                        color,
-                        [
-                        (margin + width) * column + margin,
-                        (margin + height) * row + margin,
-                        width,
-                        height
-                        ]
-                        )
+    if start_build  == True:
+        grid = generate(grid)
+    # Draw the grid
+    for row in range(rows):
+            for column in range(rows):
+                color = WHITE
+                if (grid[row][column] == 1):
+                    color = BLACK
+                pygame.draw.rect(
+                    screen,
+                    color,
+                    [
+                    (margin + width) * column + margin,
+                    (margin + height) * row + margin,
+                    width,
+                    height
+                    ]
+                    )
 
-        start_button.draw(screen, WHITE)
-        clear_button.draw(screen, WHITE)
-        upload_button.draw(screen, WHITE)
+    start_button.draw(screen, WHITE)
+    clear_button.draw(screen, WHITE)
+    upload_button.draw(screen, WHITE)
+    return grid
 
 
-
- 
 # Initialize pygame
 pygame.init()
  
 # Set the HEIGHT and WIDTH of the screen
-
 screen = pygame.display.set_mode((screen_width, screen_height))
  
 # Set title of screen
@@ -134,7 +148,8 @@ pygame.display.set_caption("Conway")
  
 # Loop until the user clicks the close button.
 done = False
-is_updating = False
+
+
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
@@ -146,7 +161,6 @@ while not done:
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
 
-        
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # User clicks the mouse. Get the position
             pos = pygame.mouse.get_pos()
@@ -156,7 +170,7 @@ while not done:
                 row = pos[1] // (height + margin)
                 # Set that location to one
            
-                grid[row][column] = 1
+                main_grid[row][column] = 1
 
                 print("Click ", pos, "Grid coordinates: ", row, column)
 
@@ -168,13 +182,11 @@ while not done:
     screen.fill(BLACK)
 
 
-    update_grid(grid, start_build)
-
-    # grid = generate(grid)
+    main_grid = update_grid(main_grid, start_build)
 
     # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-    clock.tick(10)
+    pygame.display.update()
+    clock.tick(5)
 
     # Limit to 60 frames per second
 
